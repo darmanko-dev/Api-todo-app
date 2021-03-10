d0e118ce3a2e1a9e045456ee54883edbeeec84fc
ahmeth mbacke amar <ahmeth.amar@gmail.com>
2021-03-10 21:42:26 +0000

    apiTodoApp

---
+++ src/Components/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const About = () => {
+    return ( 
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is the TodoList  app v1.0.0. Its is of a React Crash Course</p>
+        </React.Fragment>
+     );
+}
+ 
+export default About;
